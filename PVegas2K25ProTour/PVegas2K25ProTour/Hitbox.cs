47ac2b49c39504f0f9aeb7b4434f3337676c16fa
Andrew <suetholza@uwplatt.edu>
2024-03-07 15:58:53 -0600

    Added the some basic Hitbox methods Need to update the Hole to give position and radius

--- PVegas2K25ProTour/PVegas2K25ProTour/Hitbox.cs
+++ PVegas2K25ProTour/PVegas2K25ProTour/Hitbox.cs
@@ -16,19 +16,36 @@ namespace PVegas2K25ProTour
     public class Hitbox
     {
         private GraphicsDeviceManager gdm;
+        /// <summary>
+        /// Sinks up the GraphicsDevice Managers in the Constructor
+        /// </summary>
+        /// <param name="gdm">GraphicsDeviceManager from main</param>
         public Hitbox(GraphicsDeviceManager gdm)
         {
             this.gdm = gdm;
         }
+        /// <summary>
+        /// Tells when ball and Holw collide with each other
+        /// </summary>
+        /// <param name="ball">the ball for its position and radius</param>
+        /// <param name="hole">the hole for its position and radius</param>
+        /// <returns></returns>
         public bool isBallCollidingWithHole(Ball ball, Hole hole)
         {
             float radius = ball.radius()*hole.radius();
             return (!((Vector2.DistanceSquared(ball.position(), hole.position()) > (radius * radius))));
         }
-        public bool isBallOutOfbounds(Vector2 ball1Pos)
+        /// <summary>
+        /// tells when the ball "hits" the boarder 
+        /// </summary>
+        /// <param name="ball">the ball for its position and circumfrance</param>
+        /// <returns></returns>
+        public bool isBallOutOfbounds(Ball ball)
         {
-            return (ball1Pos.X <= 0 || ball1Pos.X >= gdm.PreferredBackBufferWidth
-                || ball1Pos.Y <= 0 || ball1Pos.Y >= gdm.PreferredBackBufferHeight);
+            Vector2 ballPos=ball.position();
+            float ballCircumfrance = ball.radius() * 2;
+            return (ballPos.X <= 0 || ballPos.X + ballCircumfrance >= gdm.PreferredBackBufferWidth
+                || ballPos.Y <= 0 || ballPos.Y + ballCircumfrance >= gdm.PreferredBackBufferHeight);
         }
     }
 }
