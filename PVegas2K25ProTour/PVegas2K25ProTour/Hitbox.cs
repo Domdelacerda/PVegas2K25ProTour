b1450e0576db3747bf7a45dc4803d4f083d1664a
Andrew <suetholza@uwplatt.edu>
2024-03-07 15:43:30 -0600

    Hitbox got some Stuff

--- PVegas2K25ProTour/PVegas2K25ProTour/Hitbox.cs
+++ PVegas2K25ProTour/PVegas2K25ProTour/Hitbox.cs
@@ -4,6 +4,9 @@
 // Purpose: Checks for collisions with other hitboxes every frame
 //-----------------------------------------------------------------------------
 
+using Microsoft.Xna.Framework;
+using System.Runtime.Intrinsics.Arm;
+
 namespace PVegas2K25ProTour
 {
     /// <summary>--------------------------------------------------------------
@@ -12,6 +15,20 @@ namespace PVegas2K25ProTour
     /// </summary>-------------------------------------------------------------
     public class Hitbox
     {
-
+        private GraphicsDeviceManager gdm;
+        public Hitbox(GraphicsDeviceManager gdm)
+        {
+            this.gdm = gdm;
+        }
+        public bool isBallCollidingWithHole(Ball ball, Hole hole)
+        {
+            float radius = ball.radius()*hole.radius();
+            return (!((Vector2.DistanceSquared(ball.position(), hole.position()) > (radius * radius))));
+        }
+        public bool isBallOutOfbounds(Vector2 ball1Pos)
+        {
+            return (ball1Pos.X <= 0 || ball1Pos.X >= gdm.PreferredBackBufferWidth
+                || ball1Pos.Y <= 0 || ball1Pos.Y >= gdm.PreferredBackBufferHeight);
+        }
     }
 }
