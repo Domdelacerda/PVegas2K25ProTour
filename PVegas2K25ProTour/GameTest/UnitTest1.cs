6f3be075e2745d6f8bfd61051bf3513767efd208
Cameron Pokorny <campokorny@gmail.com>
2024-04-02 23:40:17 -0500

    Created a test case for XML File saving and loading

--- PVegas2K25ProTour/GameTest/UnitTest1.cs
+++ PVegas2K25ProTour/GameTest/UnitTest1.cs
@@ -239,5 +239,28 @@ namespace GameTest
             Assert.IsTrue(golfBallReference.getSpeed().X != 0 || 
                 golfBallReference.getSpeed().Y != 0);
         }
+
+        /// <summary>----------------------------------------------------------
+        /// Checks to see if files are being correctly saved to and loaded from
+        /// an xml file by creating a playerRecord and assigning them data, 
+        /// saving and loading that data to an xml file, and assigning the 
+        /// contents of the loaded file to a new record. Then we check to make
+        /// sure that the original record is equal to the loaded record. 
+        /// </summary>---------------------------------------------------------
+        [TestMethod]
+        public void TestFileSaveAndLoad()
+        {
+            PlayerRecord my_first_player = new PlayerRecord();
+            PlayerRecord my_second_player = new PlayerRecord();
+
+            my_first_player.Strokes = 6;
+            my_first_player.User = "Bob Sullivan";
+            
+            SaveLoadSystem.Save(my_first_player);
+            my_second_player = SaveLoadSystem.Load<PlayerRecord>();
+
+            Assert.IsTrue(my_first_player.Strokes == my_second_player.Strokes &&
+                my_first_player.User == my_second_player.User);
+        }
     }
 }
