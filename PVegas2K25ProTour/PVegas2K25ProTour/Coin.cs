6ce983670f567ff2ff95d1d364b9f0da3146cb4c
Andrew <suetholza@uwplatt.edu>
2024-04-18 15:51:43 -0500

    Coins added to all the levels

--- PVegas2K25ProTour/PVegas2K25ProTour/Coin.cs
+++ PVegas2K25ProTour/PVegas2K25ProTour/Coin.cs
@@ -4,47 +4,65 @@ using Microsoft.Xna.Framework.Graphics;
 
 
 
-
 namespace PVegas2K25ProTour
 {
     public class Coin : GameObject
     {
         private Texture2D coinTexture;
         private Vector2 pos;
-        private float rad;
+        //private float rad;
         private Hitbox hitbox;
         private SpriteBatch spriteBatch;
         private Vector2 scale;
 
-        public Coin(Vector2 pos, SpriteBatch spriteBatch)
+        public Coin(Vector2 pos,SpriteBatch spriteBatch) : base(pos, spriteBatch)
         {
             this.pos = pos;
-            this.spriteBatch = spriteBatch;
+            //this.spriteBatch = spriteBatch;
             scale = Vector2.One;
+            hitbox = new Hitbox();
             
         }
         public override float  radius()
         {
-            return coinTexture.Height / 2; 
+            return (coinTexture.Height / 10) / 2; 
         }
-        public void LoadContent(ContentManager content_)
+        public virtual void LoadContent(ContentManager content_)
         {
             coinTexture = content_.Load<Texture2D>("CoinPVegas");
         }
-        public void Update(Ball ball)
+       public virtual Vector2 center()
+       {
+            Vector2 center = new Vector2();
+            center.X = pos.X;
+            center.Y = pos.Y;
+            center.X += radius();
+            center.Y += radius();
+            return center;
+       }
+        public bool Update(Ball ball)
         {
             if(hitbox.collisionCircleToCircle(ball, this))
             {
-                collide(ball);
+                //collide(ball);
+                return true;
             }
+            return false;
         }
         public virtual void collide(Ball b)
         {
-
+            pos.X = 100000000.0f;
+            pos.Y = 100000000.0f;
         }
-        public void Draw()
+        public float moneyAmount()
         {
-            spriteBatch.Draw(coinTexture, pos, Color.White);
+            return 15f;
+        }
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            this.spriteBatch = spriteBatch;
+            this.spriteBatch.Draw(coinTexture,new Rectangle((int)pos.X,(int)pos.Y,(int)coinTexture.Width/10,(int)coinTexture.Height/10), Color.White);
+
         }
     }
 }
